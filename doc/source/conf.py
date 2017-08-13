9826556b4dfe483c87cad29e018bd5f94f898b7b
Dominik Traxl <dominik.traxl@posteo.org>
2017-08-13 14:35:35 +0200

    update license.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -60,7 +60,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'DeepGraph'
-copyright = u'2016, Dominik Traxl'
+copyright = u'2017, Dominik Traxl'
 author = u'Dominik Traxl'
 
 # The version info for the project you're documenting, acts as replacement for
