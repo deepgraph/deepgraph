4a67f6921ba1b295d1dd5f9ddccb14eb5b9bbf79
Dominik Traxl <dominik.traxl@posteo.org>
2025-07-01 22:13:28 +0200

    add tests for output_names decorator.

--- tests/test_connectors_and_selectors.py
+++ tests/test_connectors_and_selectors.py
@@ -1,64 +1,145 @@
-import pytest
+import textwrap
 
+import pytest
+from unittest.mock import patch, Mock
 from deepgraph.connectors_and_selectors import CreatorFunction
+from deepgraph import output_names
+
+
+# dummy values
+x = y = z = 1
+
+
+def connector_valid(a, b, c_s, c_t):
+    r1 = (c_s - c_t) * a
+    r2 = (c_t - c_s) * b
+    return r1, r2
+
+
+@output_names("r1", "r2")
+def connector_valid_decorated(a, b, c_s, c_t):
+    r1 = (c_s - c_t) * a
+    r2 = (c_t - c_s) * b
+    return r1, r2
+
+
+def selector_valid(a, b, c_s, c_t, sources, targets):
+    r1 = (c_s - c_t) * a
+    r2 = (c_t - c_s) * b
+    sources = sources[a == b]
+    targets = targets[a == b]
+    return r1, r2, sources, targets
+
+@output_names("r1", "r2", "sources", "targets")
+def selector_valid_decorated(a, b, c_s, c_t, sources, targets):
+    r1 = (c_s - c_t) * a
+    r2 = (c_t - c_s) * b
+    sources = sources[a == b]
+    targets = targets[a == b]
+    return r1, r2, sources, targets
 
 
 class TestCreatorFunctionInit:
 
-    class TestOutputExtraction:
+    @pytest.mark.parametrize(
+        "fct, expected_name, expected_features, expected_rs",
+        [
+            (connector_valid, "connector_valid", ["c_s", "c_t"], ["a", "b"]),
+            (connector_valid_decorated, "connector_valid_decorated", ["c_s", "c_t"], ["a", "b"]),
+            (selector_valid, "selector_valid", ["c_s", "c_t"], ["a", "b"]),
+            (selector_valid_decorated, "selector_valid_decorated", ["c_s", "c_t"], ["a", "b"]),
+        ]
+    )
+    def test_input_extraction(self, fct, expected_name, expected_features, expected_rs):
+        cf = CreatorFunction(fct)
+        assert cf.fct == fct
+        assert cf.name == expected_name
+        assert cf.input_features == expected_features
+        assert cf.input_rs == expected_rs
+
+    @pytest.mark.parametrize(
+        "fct, use_mock, expected_output_rs, expected_output",
+        [
+            (connector_valid, False, ["r1", "r2"], ["r1", "r2"]),
+            (connector_valid_decorated, True, ["r1", "r2"], ["r1", "r2"]),
+            (selector_valid, False, ["r1", "r2"], ["r1", "r2", "sources", "targets"]),
+            (selector_valid_decorated, True, ["r1", "r2"], ["r1", "r2", "sources", "targets"]),
+        ]
+    )
+    def test_output_extraction(self, fct, use_mock, expected_output_rs, expected_output):
+        if use_mock:
+            with patch.object(
+                    CreatorFunction,
+                    "_extract_return_variables",
+                    wraps=CreatorFunction._extract_return_variables
+            ) as mock_extract:
+                cf = CreatorFunction(fct)
+                mock_extract.assert_not_called()
+                assert cf.output_rs == expected_output_rs
+                assert cf.output == expected_output
+        else:
+            cf = CreatorFunction(fct)
+            assert cf.output_rs == expected_output_rs
+            assert cf.output == expected_output
+
+    def test_getsource_raises_oserror(self):
+        exec_namespace = {}
+        fct_string = """
+        def connector_valid_via_exec(a, b, c_s, c_t):
+            r1 = (c_s - c_t) * a
+            r2 = (c_t - c_s) * b
+            return r1, r2
+        """
+        exec(textwrap.dedent(fct_string), exec_namespace)
+        fct = exec_namespace["connector_valid_via_exec"]
+
+        with pytest.raises(OSError) as excinfo:
+            CreatorFunction(fct)
+
+        assert "Unable to retrieve the source code of the function" in str(excinfo.value)
+        assert "connector_valid_via_exec" in str(excinfo.value)
+
+
+    class TestOutputExtractionUsingSourceCode:
 
         def test_single_variable_output(self):
-            x = 1
             def f():
                 return x
             cf = CreatorFunction(f)
             assert cf.output == ["x"]
 
         def test_multiple_variable_output(self):
-            a = b = c = 1
             def f():
-                return a, b, c
+                return x, y, z
             cf = CreatorFunction(f)
-            assert cf.output == ["a", "b", "c"]
+            assert cf.output == ["x", "y", "z"]
 
         def test_multiple_variable_explicit_tuple_output(self):
-            a = b = c = 1
             def f():
-                return (a, b, c)
+                return (x, y, z)
             cf = CreatorFunction(f)
-            assert cf.output == ["a", "b", "c"]
+            assert cf.output == ["x", "y", "z"]
 
         def test_multiple_variable_explicit_tuple_output_multiple_lines(self):
-            a = b = c = 1
             def f():
                 return (
-                    a,
-                    b,
-                    c
+                    x,
+                    y,
+                    z
                 )
             cf = CreatorFunction(f)
-            assert cf.output == ["a", "b", "c"]
-
-        def test_excludes_sources_targets_in_output_rs(self):
-            sources = targets = r1 = r2 = 1
-            def f():
-                return sources, targets, r1, r2
-            cf = CreatorFunction(f)
-            assert cf.output == ["sources", "targets", "r1", "r2"]
-            assert cf.output_rs == ["r1", "r2"]
+            assert cf.output == ["x", "y", "z"]
 
         def test_return_expression_raises(self):
-            a = b = 1
             def f():
-                return a + b
+                return x + y
             with pytest.raises(ValueError, match="Return must consist of variable names only."):
                 CreatorFunction(f)
 
         def test_return_function_call_raises(self):
             func = lambda x: x
-            a = 1
             def f():
-                return func(a)
+                return func(y)
             with pytest.raises(ValueError, match="Return must consist of variable names only."):
                 CreatorFunction(f)
 
@@ -69,7 +150,6 @@ class TestCreatorFunctionInit:
                 CreatorFunction(f)
 
         def test_tuple_with_expression_raises(self):
-            x = y = z = 1
             def f():
                 return x, y + z
             with pytest.raises(ValueError, match="Return tuple must contain only variable names."):
